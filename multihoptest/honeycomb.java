71e3098a6fcf13f73259b05c57bce33c2592e920
breeze <448031516@qq.com>
2019-03-15 12:36:58 +0800

    none

--- multihoptest/honeycomb.java
+++ multihoptest/honeycomb.java
@@ -8,13 +8,18 @@ public class honeycomb {
     public  honeycomb(){
         double x=0;
         double y=0;
-        int p=0;
-        for(;y<=networkSize;){
-            for(;x<=networkSize;){
-                location[p]=new Point(x,y);
+        int p=0,xi=0,yi=0;
+        for(;x<=networkSize;){
+            for(;y<=networkSize;){
+                if(xi%2== 0&&yi%2==0) {
+                    y=y+yi*edge;
+                    x=x+xi*edge*Math.sqrt(3)/2;
+                    location[p]=new Point(x,y);
+                }
                 p++;
-                x+=2*edge;
+
             }
+
         }
 
     }
