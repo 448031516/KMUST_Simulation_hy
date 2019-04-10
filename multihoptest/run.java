4139beab6c56e28e3769b23794eaa47a8042b8cd
breeze <448031516@qq.com>
2019-04-11 00:58:17 +0800

    初次添加tsp

--- multihoptest/run.java
+++ multihoptest/run.java
@@ -1,5 +1,7 @@
 package multihoptest;
 
+
+
 public class run {
 
     public static void main(String[] args){
@@ -53,5 +55,53 @@ public class run {
 //            System.out.println("第"+Anchor[i].num+"个簇的锚点坐标为（"+Anchor[i].x+"，"+Anchor[i].y+"),且以此为锚点，MC能覆盖到的节点个数为："+num);
         }
 
+        int[] best;
+
+        //=======================method 1=======================
+        GeneticAlgorithm ga = new GeneticAlgorithm();
+        best = ga.tsp(getDist(Anchor));
+
+        int n = 0;
+        while (n++ < 100) {
+            best = ga.nextGeneration();
+
+            System.out.println("best distance:" + ga.getBestDist() +
+                    " current generation:" + ga.getCurrentGeneration() +
+                    " mutation times:" + ga.getMutationTimes());
+            System.out.print("best path:");
+            for (int i = 0; i < best.length; i++) {
+                System.out.print(best[i] + " ");
+            }
+            System.out.println();
+        }
+
+        //=======================method 2========================
+//        GeneticAlgorithm ga = GeneticAlgorithm.getInstance();
+//
+//        ga.setMaxGeneration(1000);
+//        ga.setAutoNextGeneration(true);
+//        best = ga.tsp(getDist(Anchor));
+//        System.out.print("best path:");
+//        for (int i = 0; i < best.length; i++) {
+//            System.out.print(best[i] + " ");
+//        }
+//        System.out.println();
     }
+
+    private static float[][] getDist(Point[] points) {
+        float[][] dist = new float[points.length][points.length];
+        for (int i = 0; i < points.length; i++) {
+            for (int j = 0; j < points.length; j++) {
+                dist[i][j] = (float) Point.getDistance(points[i], points[j]);
+            }
+        }
+        return dist;
+    }
+
+//    private static float distance(com.onlylemi.genetictsp.Point p1, com.onlylemi.genetictsp.Point p2) {
+//        return (float) Math.sqrt((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y));
+//    }
+
+
+
 }
