d3b6893ac35fd70f11d86b0f31640cffa0e90f5a
breeze <448031516@qq.com>
2019-04-12 11:09:36 +0800

    tsp

--- multihoptest/run.java
+++ multihoptest/run.java
@@ -1,10 +1,11 @@
 package multihoptest;
+import java.util.Arrays;
 
 
 
 public class run {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         //网络规模
         double networkSize = 200;
         //传感器节点个数
@@ -22,11 +23,11 @@ public class run {
 //        for(Sensor node:allSensor) {
 //            System.out.println("编号:"+node.number+" 坐标:("+node.location.x+","+node.location.y+") 剩余能量阈值:"+node.remainingE/node.maxCapacity +" 剩余寿命:"+node.remainingE/node.ecRate);
 //        }
-        honeycomb test =new honeycomb(8.00,210.00);    //创建蜂窝
-        Point[] k =test.creat_honeycomb(8.00,210.00);//获取每个蜂窝的中心坐标
+        honeycomb test = new honeycomb(8.00, 210.00);    //创建蜂窝
+        Point[] k = test.creat_honeycomb(8.00, 210.00);//获取每个蜂窝的中心坐标
 
-        for(int i=0;i<allSensor.length;i++){                //将节点分簇，每个节点的inHoneycomb值表示其所在的簇
-            allSensor[i].inHoneycomb = WsnFunction.judgeHoneycomb(allSensor[i],test);
+        for (int i = 0; i < allSensor.length; i++) {                //将节点分簇，每个节点的inHoneycomb值表示其所在的簇
+            allSensor[i].inHoneycomb = WsnFunction.judgeHoneycomb(allSensor[i], test);
             //System.out.println("编号:"+allSensor[i].number+" 坐标:("+allSensor[i].location.x+","+allSensor[i].location.y+")属于编号为"+allSensor[i].inHoneycomb+"的正六边形，其中心坐标为"+"("+k[WsnFunction.judgeHoneycomb(allSensor[i],test)].x+","+k[WsnFunction.judgeHoneycomb(allSensor[i],test)].y+")");
         }
 
@@ -41,20 +42,36 @@ public class run {
 //        int number = WsnFunction.cloverNUM(initialAnchor[11][3],allSensor);
 //        System.out.println(number);
 
-        for (int i=0;i<Anchor.length;i++){
-            int num=0,temp_1=0;
-            for (int j=0;j<6;j++) {
-                if(num < WsnFunction.cloverNUM(initialAnchor[i][j],allSensor)){
-                    num = WsnFunction.cloverNUM(initialAnchor[i][j],allSensor);
-                    temp_1=j;
+        for (int i = 0; i < Anchor.length; i++) {
+            int num = 0, temp_1 = 0;
+            for (int j = 0; j < 6; j++) {
+                if (num < WsnFunction.cloverNUM(initialAnchor[i][j], allSensor)) {
+                    num = WsnFunction.cloverNUM(initialAnchor[i][j], allSensor);
+                    temp_1 = j;
                 }
             }
 //            System.out.println(i+","+temp_1+","+num);
-            Anchor[i]=initialAnchor[i][temp_1];    //确定第i个正六边形区域的锚点坐标
-//            //测试输出
-//            System.out.println("第"+Anchor[i].num+"个簇的锚点坐标为（"+Anchor[i].x+"，"+Anchor[i].y+"),且以此为锚点，MC能覆盖到的节点个数为："+num);
-        }
+            if(num==0)  Anchor[i] = new Point();
+             else   Anchor[i] = initialAnchor[i][temp_1];    //确定第i个正六边形区域的锚点坐标
 
+            //测试输出
+            //System.out.println("第"+Anchor[i].num+"个簇的锚点坐标为（"+Anchor[i].x+"，"+Anchor[i].y+"),且以此为锚点，MC能覆盖到的节点个数为："+num);
+        }
+        int num_temp=0;
+        for (int i=0;i<Anchor.length;i++){
+            if (Anchor[i].num==0) {
+                num_temp++;
+                for(int j=i;j<Anchor.length-1;j++)
+                Anchor[j]=Anchor[j+1];
+            }
+        }
+        Anchor = Arrays.copyOf(Anchor, Anchor.length-18);
+        for (Point node:Anchor
+             ) {
+            System.out.println("第"+node.num+"个簇的锚点坐标为（"+node.x+"，"+node.y+"),且以此为锚点，MC能覆盖到的节点个数为：");
+
+        }
+/*
         //TSP开始
         int[] best;
 
@@ -103,6 +120,6 @@ public class run {
 //        return (float) Math.sqrt((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y));
 //    }
 
+*/
 
-
-}
+    }}
